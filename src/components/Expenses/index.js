319960ee6b35cbcea324c0d0fb2234d9a8a1fbdd
Sam van Erpers Roijaards <samvanerpersroijaards@gmail.com>
2022-10-11 08:05:56 +1300

    Adds WIP expense filter

--- src/components/Expenses/index.js
+++ src/components/Expenses/index.js
@@ -1,10 +1,19 @@
 import './style.css'
+import { useState } from 'react';
 import { ExpenseItem } from './ExpenseItem'
 import { Card } from '../UI/Card'
+import { ExpensesFilter } from './ExpenseFilter'
 
 export const Expenses = ({ expenses }) => {
+
+  const [ year, setYear ] = useState('') 
+  const handleFilter = (selectedYear) => {
+    setYear(selectedYear);
+  }
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter year={year} onSelect={handleFilter} />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}

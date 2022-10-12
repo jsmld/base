e37091a01578a7155fa5707f9898cc59f60e84d5
Sam van Erpers Roijaards <samvanerpersroijaards@gmail.com>
2022-10-12 16:16:51 +1300

    Renders conditional message if no expenses

--- src/components/Expenses/index.js
+++ src/components/Expenses/index.js
@@ -6,18 +6,23 @@ import { ExpensesFilter } from './ExpenseFilter'
 
 export const Expenses = ({ expenses }) => {
 
-  const [year, setYear] = useState('2020')
+  const [year, setYear] = useState('2022')
   const handleFilter = (selectedYear) => {
     setYear(selectedYear);
   }
   const filteredExpenses = expenses.filter(expense => expense.date.getFullYear().toString() === year)
 
+  let expensesContent = <p style={{color: '#FFF', paddingLeft: '15px'}}>No expenses found.</p>
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(({ title, amount, date, id }) => (
+      <ExpenseItem key={id} title={title} amount={amount} date={date} />
+    ))
+  }
+
   return (
     <Card className='expenses'>
       <ExpensesFilter year={year} onSelect={handleFilter} />
-      {filteredExpenses.map(({ title, amount, date, id }) => (
-        <ExpenseItem key={id} title={title} amount={amount} date={date} />
-      ))}
+      {expensesContent}
     </Card>
   )
 }
